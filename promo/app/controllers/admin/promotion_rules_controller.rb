334b728441942cfab93e5e493ff33f9d58acc888
David Aaron Fendley <tricon@me.com>
2012-02-03 03:59:35 -0600

    Added mass assignmnet fix.

--- promo/app/controllers/admin/promotion_rules_controller.rb
+++ promo/app/controllers/admin/promotion_rules_controller.rb
@@ -1,7 +1,11 @@
 class Admin::PromotionRulesController < Admin::BaseController
   def create
     @promotion = Promotion.find(params[:promotion_id])
-    @promotion_rule = params[:promotion_rule][:type].constantize.new(params[:promotion_rule])
+    # Remove type key from this hash so that we don't attempt
+    # to set it when creating a new record, as this is raises
+    # an error in ActiveRecord 3.2.
+    promotion_rule_type = params[:promotion_rule].delete(:type)
+    @promotion_rule = promotion_rule_type.constantize.new(params[:promotion_rule])
     @promotion_rule.promotion = @promotion
     if @promotion_rule.save
       flash[:notice] = I18n.t(:successfully_created, :resource => I18n.t(:promotion_rule))
@@ -11,7 +15,7 @@ class Admin::PromotionRulesController < Admin::BaseController
       format.js   { render :layout => false }
     end
   end
-  
+
   def destroy
     @promotion = Promotion.find(params[:promotion_id])
     @promotion_rule = @promotion.promotion_rules.find(params[:id])
@@ -21,6 +25,6 @@ class Admin::PromotionRulesController < Admin::BaseController
     respond_to do |format|
       format.html { redirect_to edit_admin_promotion_path(@promotion)}
       format.js   { render :layout => false }
-    end  
+    end
   end
 end
